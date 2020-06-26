72337105ca7e36968edff76cf1a69de706698452
varunbm <bmvnaik3@gmail.com>
2020-06-26 11:34:16 +0530

    Changes

--- backend/config/key.js
+++ backend/config/key.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   console.log("Node env production..");
   mongoUri: process.env.MONGO_URI;
-  console.log("----------->>>>>>", mongoUri.MONGO_URI);
+  console.log("----------->>>>>>", process.env.MONGO_URI);
   // module.exports = require("./prod");
 } else {
   console.log("Dev");
